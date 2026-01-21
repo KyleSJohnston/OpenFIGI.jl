d2ecc98680d5d85b11a6dd0773bdae5b5efc8d7a
Kyle S. Johnston <KyleSJohnston@users.noreply.github.com>
2026-01-21 10:33:12 -0500

    set/get apikey global

--- src/OpenFIGI.jl
+++ src/OpenFIGI.jl
@@ -1,5 +1,26 @@
 module OpenFIGI
 
-greet() = print("Hello World!")
+public set_apikey
+
+const APIKEY = Ref{Union{Nothing, String}}(nothing)
+
+"""
+    set_apikey(apikey)
+
+Sets the (global) apikey to `apikey`
+"""
+function set_apikey(apikey::Union{AbstractString, Nothing})
+    APIKEY[] = isnothing(apikey) ? apikey : String(apikey)
+end
+
+"""
+    get_apikey()
+
+Retrieves the (global) apikey
+"""
+function get_apikey()
+    return APIKEY[]
+end
+
 
 end # module OpenFIGI
---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using OpenFIGI
+using Test
+
+@testset "apikey" begin
+    @test isnothing(OpenFIGI.get_apikey())
+    testkey = "api-key-for-testing-purposes"
+    OpenFIGI.set_apikey(testkey)
+    @test OpenFIGI.get_apikey() == testkey
+    OpenFIGI.set_apikey(nothing)
+    @test isnothing(OpenFIGI.get_apikey())
+end
